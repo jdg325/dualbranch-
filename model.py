cbdca0003357a3cb70532f5e2a86e4cbdbc38170
jdg325 <86508441+jdg325@users.noreply.github.com>
2025-01-24 11:12:31 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,430 @@
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+from functools import partial
+import torch.nn.functional as F
+import math
+from timm.models.vision_transformer import _cfg
+from timm.models.registry import register_model
+from timm.models.layers import trunc_normal_, DropPath, to_2tuple
+from einops.einops import rearrange
+from torch import Tensor, LongTensor
+from typing import Tuple, Optional
+import time
+from torch.nn import init
+import math
+
+class DWConv(nn.Module):
+    def __init__(self, dim=768):
+        super(DWConv, self).__init__()
+        self.dwconv = nn.Conv2d(dim, dim, 3, 1, 1, bias=True, groups=dim)
+
+    def forward(self, x):
+        x = self.dwconv(x)
+        return x
+
+
+
+
+class SSA(nn.Module):
+    '''
+    Scaled dot-product attention
+    '''
+
+    def __init__(self, d_model):
+        '''
+        :param d_model: Output dimensionality of the model
+        :param d_k: Dimensionality of queries and keys
+        :param d_v: Dimensionality of values
+        :param h: Number of heads
+        '''
+        super(MobileViTv2Attention, self).__init__()
+        self.fc_i = nn.Linear(d_model, 1)
+        self.fc_k = nn.Linear(d_model, d_model)
+        self.fc_v = nn.Linear(d_model, d_model)
+        self.fc_o = nn.Linear(d_model, d_model)
+
+        self.d_model = d_model
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, input):
+        '''
+        Computes
+        :param queries: Queries (b_s, nq, d_model)
+        :return:
+        '''
+        i = self.fc_i(input)  # (bs,nq,1)
+        weight_i = torch.softmax(i, dim=1)  # bs,nq,1
+        context_score = weight_i * self.fc_k(input)  # bs,nq,d_model
+        context_vector = torch.sum(context_score, dim=1, keepdim=True)  # bs,1,d_model
+        v = self.fc_v(input) * context_vector  # bs,nq,d_model
+        out = self.fc_o(v)  # bs,nq,d_model
+
+        return out
+
+
+
+def to_3d(x):
+    return rearrange(x, 'b c h w -> b (h w) c')
+
+def to_4d(x, h, w):
+    return rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+
+
+
+class Alpffn(nn.Module):
+    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.,
+                 kernel_size=3, with_bn=True):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        # pointwise
+        self.conv1 = nn.Conv2d(in_features, hidden_features, kernel_size=1, stride=1, padding=0)
+        # depthwise
+        self.conv2 = nn.Conv2d(
+            hidden_features, hidden_features, kernel_size=kernel_size, stride=1,
+            padding=(kernel_size - 1) // 2, groups=hidden_features
+        )
+        # pointwise
+        self.conv3 = nn.Conv2d(hidden_features, out_features, kernel_size=1, stride=1, padding=0)
+        self.act = act_layer()
+        # self.drop = nn.Dropout(drop)
+
+        self.with_bn = with_bn
+        if self.with_bn:
+            self.bn1 = nn.BatchNorm2d(hidden_features)
+            self.bn2 = nn.BatchNorm2d(hidden_features)
+            self.bn3 = nn.BatchNorm2d(out_features)
+
+    def forward(self, x):
+        b, n, k = x.size()
+        # cls_token, tokens = torch.split(x, [1, n - 1], dim=1)
+        x = x.reshape(b, int(math.sqrt(n )), int(math.sqrt(n )), k).permute(0, 3, 1, 2)
+        if self.with_bn:
+            x = self.conv1(x)
+            x = self.bn1(x)
+            x = self.act(x)
+            x = self.conv2(x)
+            x = self.bn2(x)
+            x = self.act(x)
+            x = self.conv3(x)
+            x = self.bn3(x)
+        else:
+            x = self.conv1(x)
+            x = self.act(x)
+            x = self.conv2(x)
+            x = self.act(x)
+            x = self.conv3(x)
+
+        x = x.flatten(2).permute(0, 2, 1)
+        # out = torch.cat((cls_token, tokens), dim=1)
+        return (x + x.mean(dim=1, keepdim=True)) * 0.5
+
+
+
+
+
+class Partial_conv3(nn.Module):
+
+    def __init__(self, dim, n_div, forward):
+        super().__init__()
+        self.dim_conv3 = dim // n_div
+        self.dim_untouched = dim - self.dim_conv3
+        # self.partial_conv3 = nn.Conv2d(self.dim_conv3, self.dim_conv3, 5, 1, 2, bias=False)
+        self.partial_conv3 = nn.Conv2d(self.dim_conv3, self.dim_conv3, 3, 1, 1, bias=False)
+
+        if forward == 'slicing':
+            self.forward = self.forward_slicing
+        elif forward == 'split_cat':
+            self.forward = self.forward_split_cat
+        else:
+            raise NotImplementedError
+
+    def forward_slicing(self, x):
+        # only for inference
+        x = x.clone()  # !!! Keep the original input intact for the residual connection later
+        x[:, :self.dim_conv3, :, :] = self.partial_conv3(x[:, :self.dim_conv3, :, :])
+
+        return x
+
+    def forward_split_cat(self, x):
+        # for training/inference
+        x1, x2 = torch.split(x, [self.dim_conv3, self.dim_untouched], dim=1)
+        x1 = self.partial_conv3(x1)
+        x = torch.cat((x1, x2), 1)
+
+        return x
+
+
+
+
+
+
+
+class SelfAttn(nn.Module):
+    def __init__(self, dim, num_heads=8, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, sr_ratio=1.):
+        super().__init__()
+        self.pos_embed = nn.Conv2d(dim, dim, 3, padding=1, groups=dim)
+        self.norm1 = norm_layer(dim)
+        self.attn = SSA(dim)
+        # NOTE: drop path for stochastic depth, we shall see if this is better than dropout here
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+        self.norm2 = norm_layer(dim)
+        mlp_hidden_dim = int(dim * mlp_ratio )
+        self.mlp = Alpffn(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
+        self.spatial_mixing = Partial_conv3(
+            dim,
+            4,
+            'split_cat'
+        )
+        cnn_hidden_dim = int(dim * mlp_ratio)
+
+        mlp_layer: List[nn.Module] = [
+            nn.Conv2d(dim, cnn_hidden_dim, 1, bias=False),
+            nn.BatchNorm2d(cnn_hidden_dim),
+            act_layer(),
+            nn.Conv2d(cnn_hidden_dim, dim, 1, bias=False)
+        ]
+
+        self.cnn_mlp = nn.Sequential(*mlp_layer)
+        
+        self.weight_a = nn.Parameter(torch.Tensor([0.5]))
+        # self.fusion_conv = nn.Conv2d(2 * dim, dim, kernel_size=1, stride=1, bias=False)
+
+
+       
+
+    def forward(self, x):
+        
+        x_cnn = x
+        x_cnn = x.clone()
+        x_transformer = x
+
+        #transformer分支
+        x_transformer = x_transformer+ self.pos_embed(x_transformer)
+        B, N, H, W = x_transformer.shape
+        x_transformer = x_transformer.flatten(2).transpose(1, 2)
+        x_transformer = x_transformer + self.drop_path(self.attn(self.norm1(x_transformer)))
+        x_transformer = x_transformer + self.drop_path(self.mlp(self.norm2(x_transformer)))
+        x_transformer = x_transformer.transpose(1, 2).reshape(B, N, H, W)
+
+        # #cnn分支
+        x_cnn = self.spatial_mixing(x_cnn)
+        x_cnn = self.cnn_mlp(x_cnn)
+
+        # x_fused = torch.cat([x_transformer, x_cnn], dim=1)
+
+#         # 使用 1x1 卷积映射回原始维度
+        # x_fused = self.fusion_conv(x_fused)
+        
+        x_fused = self.weight_a * x_transformer + (1 - self.weight_a) * x_cnn
+        # x_fused =  x_transformer +  x_cnn
+
+        return   x_fused
+
+class GLUBlock(nn.Module):
+    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
+                 drop_path=0., act_layer=nn.GELU, norm_layer=nn.LayerNorm, sr_ratio=1.):
+        super().__init__()
+        
+#
+        self.SelfAttn = SelfAttn(dim, num_heads, mlp_ratio, qkv_bias, qk_scale, drop, attn_drop, drop_path, act_layer, norm_layer, sr_ratio)
+    
+    def forward(self, x):
+
+        x = self.SelfAttn(x)
+        return x
+
+
+class PatchEmbed(nn.Module):
+    """ Image to Patch Embedding
+    """
+    def __init__(self, img_size=256, patch_size=16, in_chans=3, embed_dim=768):
+        super().__init__()
+        img_size = to_2tuple(img_size)
+        patch_size = to_2tuple(patch_size)
+        num_patches = (img_size[1] // patch_size[1]) * (img_size[0] // patch_size[0])
+        self.img_size = img_size
+        self.patch_size = patch_size
+        self.num_patches = num_patches
+        self.norm = nn.LayerNorm(embed_dim)
+        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, x):
+        B, C, H, W = x.shape
+        assert H == self.img_size[0] and W == self.img_size[1], \
+            f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
+        x = self.proj(x)
+        B, C, H, W = x.shape
+        x = x.flatten(2).transpose(1, 2)
+        x = self.norm(x)
+        x = x.reshape(B, H, W, -1).permute(0, 3, 1, 2).contiguous()
+        return x
+
+
+class Model(nn.Module):
+    """ Vision Transformer
+    A PyTorch impl of : `An Image is Worth 16x16 Words: Transformers for Image Recognition at Scale`  -
+        https://arxiv.org/abs/2010.11929
+    """
+    def __init__(self, depth=[1, 1, 3, 2], img_size=224, in_chans=3, num_classes=3, embed_dim=[36, 72, 144, 288],
+                 head_dim=36, mlp_ratio=[4]*4, qkv_bias=True, qk_scale=None, representation_size=None,
+                 drop_rate=0., attn_drop_rate=0., drop_path_rate=0., norm_layer=None, sr_ratios=[4,2,2,1], **kwargs):
+        """
+        Args:
+            depth (list): depth of each stage
+            img_size (int, tuple): input image size
+            in_chans (int): number of input channels
+            num_classes (int): number of classes for classification head
+            embed_dim (list): embedding dimension of each stage
+            head_dim (int): head dimension
+            mlp_ratio (int): ratio of mlp hidden dim to embedding dim
+            qkv_bias (bool): enable bias for qkv if True
+            qk_scale (float): override default qk scale of head_dim ** -0.5 if set
+            representation_size (Optional[int]): enable and set representation layer (pre-logits) to this value if set
+            drop_rate (float): dropout rate
+            attn_drop_rate (float): attention dropout rate
+            drop_path_rate (float): stochastic depth rate
+            norm_layer (nn.Module): normalization layer
+        """
+        super().__init__()
+        self.num_classes = num_classes
+        self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
+        norm_layer = norm_layer or partial(nn.LayerNorm, eps=1e-6)
+
+        self.patch_embed1 = PatchEmbed(
+            img_size=img_size, patch_size=4, in_chans=in_chans, embed_dim=embed_dim[0])
+        self.patch_embed2 = PatchEmbed(
+            img_size=img_size // 4, patch_size=2, in_chans=embed_dim[0], embed_dim=embed_dim[1])
+        self.patch_embed3 = PatchEmbed(
+            img_size=img_size // 8, patch_size=2, in_chans=embed_dim[1], embed_dim=embed_dim[2])
+        self.patch_embed4 = PatchEmbed(
+            img_size=img_size // 16, patch_size=2, in_chans=embed_dim[2], embed_dim=embed_dim[3])
+
+        self.pos_drop = nn.Dropout(p=drop_rate)
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, sum(depth))]  # stochastic depth decay rule
+        num_heads = [dim // head_dim for dim in embed_dim]
+        self.blocks1 = nn.ModuleList([
+            GLUBlock(
+                dim=embed_dim[0], num_heads=num_heads[0], mlp_ratio=mlp_ratio[0], qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer, sr_ratio=sr_ratios[0])
+            for i in range(depth[0])])
+        self.blocks2 = nn.ModuleList([
+            GLUBlock(
+                dim=embed_dim[1], num_heads=num_heads[1], mlp_ratio=mlp_ratio[1], qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i+depth[0]], norm_layer=norm_layer, sr_ratio=sr_ratios[1])
+            for i in range(depth[1])])
+        self.blocks3 = nn.ModuleList([
+            GLUBlock(
+                dim=embed_dim[2], num_heads=num_heads[2], mlp_ratio=mlp_ratio[2], qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i+depth[0]+depth[1]], norm_layer=norm_layer, sr_ratio=sr_ratios[2])
+            for i in range(depth[2])])
+        self.blocks4 = nn.ModuleList([
+            GLUBlock(
+                dim=embed_dim[3], num_heads=num_heads[3], mlp_ratio=mlp_ratio[3], qkv_bias=qkv_bias, qk_scale=qk_scale,
+                drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i+depth[0]+depth[1]+depth[2]], norm_layer=norm_layer, sr_ratio=sr_ratios[3])
+            for i in range(depth[3])])
+        self.norm = nn.BatchNorm2d(embed_dim[-1])
+
+        # Representation layer
+        if representation_size:
+            self.num_features = representation_size
+            self.pre_logits = nn.Sequential(OrderedDict([
+                ('fc', nn.Linear(embed_dim, representation_size)),
+                ('act', nn.Tanh())
+            ]))
+        else:
+            self.pre_logits = nn.Identity()
+
+        # Classifier head
+        self.head = nn.Linear(embed_dim[-1], num_classes) if num_classes > 0 else nn.Identity()
+
+        self.apply(self._init_weights)
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'pos_embed', 'cls_token'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes, global_pool=''):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+        x = self.patch_embed1(x)
+        x = self.pos_drop(x)
+        for blk in self.blocks1:
+            x = blk(x)
+        x = self.patch_embed2(x)
+        for blk in self.blocks2:
+            x = blk(x)
+        x = self.patch_embed3(x)
+        for blk in self.blocks3:
+            x = blk(x)
+        x = self.patch_embed4(x)
+        for blk in self.blocks4:
+            x = blk(x)
+        x = self.norm(x)
+        x = self.pre_logits(x)
+        return x
+
+    def forward(self, x):
+        x = self.forward_features(x)
+        x = x.flatten(2).mean(-1)
+        x = self.head(x)
+        return x
+
+
+@register_model
+def edgevit_xxs(pretrained=True, **kwargs):
+    model = EdgeVit(
+        depth=[1, 1, 3, 2],
+        embed_dim=[36, 72, 144, 288], head_dim=36, mlp_ratio=[4]*4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), sr_ratios=[4,2,2,1], **kwargs)
+    model.default_cfg = _cfg()
+    return model
+
+@register_model
+def edgevit_xs(pretrained=True, **kwargs):
+    model = EdgeVit(
+        depth=[1, 1, 3, 1],
+        embed_dim=[48, 96, 240, 384], head_dim=48, mlp_ratio=[4]*4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), sr_ratios=[4,2,2,1], **kwargs)
+    model.default_cfg = _cfg()
+    return model
+
+
+@register_model
+def edgevit_s(pretrained=True, **kwargs):
+    model = EdgeVit(
+        depth=[1, 2, 5, 3],
+        embed_dim=[48, 96, 240, 384], head_dim=48, mlp_ratio=[4]*4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), sr_ratios=[4,2,2,1], **kwargs)
+    model.default_cfg = _cfg()
+    return model
